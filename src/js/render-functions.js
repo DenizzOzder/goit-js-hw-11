7b54f9188fab0b4f9d80815d2026e867a31221ea
Deniz <candenizozder08@gmail.com>
2025-05-28 10:23:53 +0300

    fixes

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,27 +1,25 @@
-import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import iziToast from 'izitoast';
-
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 let lightbox = null;
 export function renderGallery(images) {
-    return images.map(image => {
-      return `
-        <li class="gallery-item">
-          <a href="${image.largeImageURL}">
-            <img src="${image.webformatURL}" alt="${image.tags}" />
-          </a>
-          <div class="info">
-            <p>Likes: ${image.likes}</p>
-            <p>Views: ${image.views}</p>
-            <p>Comments: ${image.comments}</p>
-            <p>Downloads: ${image.downloads}</p>
-          </div>
-        </li>
-      `;
-    }).join('');
+      const markup = images
+    .map(
+      image => `
+      <a href="${image.largeImageURL}" class="gallery-item">
+        <img src="${image.webformatURL}" alt="${image.tags}" />
+        <div class="info">
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+      </a>
+    `
+    )
+    .join('');
+
     gallery.insertAdjacentHTML('beforeend',markup);
 
     if (!lightbox) {
@@ -30,11 +28,9 @@ export function renderGallery(images) {
       lightbox.refresh();
     }
   }
-
   export function clearGallery() {
     gallery.innerHTML = '';
   }
-
   export function showLoader() {
   loader.classList.remove('hidden');
 }
--- src/main.js
+++ src/main.js
@@ -1,48 +1,45 @@
 import { fetchImages } from './js/pixabay-api.js';
 import { renderGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js'; 
 import iziToast from 'izitoast';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
 
-  const form = document.querySelector('#search-form');
-  const gallery = document.querySelector('.gallery');
-  const loader = document.querySelector('.loader');
-  let lightbox = new SimpleLightbox('.gallery a');
+const form = document.querySelector('#searchForm');
+const input = document.querySelector('#searchInput');
 
   form.addEventListener('submit', async e => {
     e.preventDefault();
-    const query = e.target.elements.searchQuery.value.trim();
+    const query = input.value.trim();
   
-    if (!query) {
-      iziToast.error({
-        title: 'Oops',
-        message: `Sorry, there are no images matching your search. Please try again!`,
-      });
-      lightbox.refresh();
-      return
-    }
-    clearGallery();
-    showLoader();
-    hideLoader();
+   if (!query) return;
+
+  clearGallery();   
+  showLoader();  
   
-    try {
-      const data = await fetchImages(query);
-      showLoader();
-  
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: 'Oops',
-          message: `Sorry, there are no images matching your search ${e.target.elements.searchQuery.value}. Please try again!`,
+      fetchImages(query)
+    .then(response => {
+      const images = response.data.hits;
+
+      if (images.length === 0) {
+        iziToast.info({
+          title: 'No Results',
+          message: 'Sorry, there are no images matching your search query. Please try again!',
         });
+        hideLoader(); 
         return;
       }
-  
-      const markup = renderGallery(data.hits);
-      gallery.innerHTML = markup;
-  
-      lightbox.refresh();
-    } catch (error) {
-      loader.classList.add('hidden');
-      iziToast.error({ title: 'Hata', message: 'Görseller alınamadı!' });
-    }
+
+      
+      setTimeout(() => {
+        renderGallery(images);
+        hideLoader();
+      }, 1000);
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+      iziToast.error({
+        title: 'Error',
+        message: 'An error occurred while fetching data.',
+      });
+      hideLoader();
+    });
   });
